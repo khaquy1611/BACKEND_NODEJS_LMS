46f2c653e99d86d17e56745585097c5f0cd6a028
quynk <khaquy12a2@gmail.com>
2025-04-24 14:46:54 +0700

    init typescript express and write first routes

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+  res.send('Check ABC!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
