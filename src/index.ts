8024228b30ab9aea5e19f7ea91ec49254204722e
quynk <khaquy12a2@gmail.com>
2025-04-24 15:24:30 +0700

    make env file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3000
+const hostName = process.env.HOST_NAME || 'localhost'
+// config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('sample')
 })
 
 app.get('/abc', (req, res) => {
@@ -12,5 +19,5 @@ app.get('/abc', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on ${hostName}/${port}`)
 })
